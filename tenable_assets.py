ddc9768f76b7942d41d8b79555a9e79e0b315e67
contrast-dan <daniel.afkhami-ardekani@contrastsecurity.com>
2024-11-20 11:11:38 -0500

    create file 'tenable_assets.py.' test tenable api connection.

---
+++ tenable_assets.py
@@ -0,0 +1,38 @@
+from tenable.io import TenableIO
+
+# Get access/secret key for SES
+def get_secret():
+
+    secret_name = "ses_tenable_key"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+# Initiate tenable authorization
+tio = TenableIO(secret_name)
+
+#Get all assets
+assets = tio.assets.list()
+
+# Extract all machine names
+machine_names = [asset['hostname'] for asset in assets]
+
+# Print machine names
+for name in machine_names:
+	print(name)
